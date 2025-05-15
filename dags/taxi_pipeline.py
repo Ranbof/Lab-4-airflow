974085591045d95c07cfebe1af6ff422503a9c8a
Ranbof <168290421+Ranbof@users.noreply.github.com>
2025-05-15 23:47:54 +0500

    Update taxi_pipeline.py

--- dags/taxi_pipeline.py
+++ dags/taxi_pipeline.py
@@ -6,20 +6,17 @@ from train_taxi_model import train_taxi_model
 
 def download_taxi_data():
     df = pd.read_csv('https://raw.githubusercontent.com/Ranbof/Data-lab-3/refs/heads/main/taxi_dataset.csv')
-    #df.to_csv("taxi_data.csv", index=False)
     print(f"Downloaded data shape: {df.shape}")
     return True
 
 def preprocess_taxi_data():
     df = pd.read_csv("https://raw.githubusercontent.com/Ranbof/Data-lab-3/refs/heads/main/taxi_dataset.csv")
     
-    # Basic cleaning
     df = df.dropna()
     df = df[(df['trip_distance'] > 0) & (df['trip_distance'] < 100)]
     df = df[(df['total_amount'] > 0) & (df['total_amount'] < 200)]
     df = df[df['passenger_count'] > 0]
     
-    # Feature engineering
     df['pickup_hour'] = pd.to_datetime(df['tpep_pickup_datetime']).dt.hour
     df['pickup_day'] = pd.to_datetime(df['tpep_pickup_datetime']).dt.dayofweek
     df = df.drop(['tpep_pickup_datetime', 'tpep_dropoff_datetime'], axis=1)
